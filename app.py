e85c94428107a815b0da2e81c211572e4b82af38
mrcbrbn5361 <brtapardi@gmail.com>
2025-02-05 15:44:56 +0300

    Port ayarları güncellendi

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -9,4 +10,5 @@ def home():
     return render_template('index.html', mesaj=mesaj)
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host='0.0.0.0', port=port) 
